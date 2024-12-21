f3976f201a788f5dce5b7e0f076ac78a023cdb53
prayagprasad <145901630+prayagprasad@users.noreply.github.com>
2024-12-21 05:14:51 +0000

    Add files via upload

---
+++ week9/calculator-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import KeyPadComponent from './Calculator';
+
+function App() {
+    return (
+        <div>
+            <KeyPadComponent />
+        </div>
+    );
+}
+
+export default App;
