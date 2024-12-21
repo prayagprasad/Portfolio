5da23eb5cdcecca1bca2da798035107a3416bd59
prayagprasad <145901630+prayagprasad@users.noreply.github.com>
2024-12-21 05:05:26 +0000

    Add files via upload

---
+++ week1/week1.js
@@ -0,0 +1,17 @@
+console.log("This is my first program");
+console.log("Welcome John, your monthly salary is 500000");
+
+const Num1 = 5;
+const Num2 = 3;
+
+
+const Add = Num1 + Num2;
+const Sub = Num1 - Num2;
+const Mul = Num1 * Num2;
+const Div = Num1 / Num2;
+
+
+console.log('The Addition of ' + Num1 + ' and ' + Num2 + ' is: ' + Add);
+console.log('The Subtraction of ' + Num1 + ' and ' + Num2 + ' is: ' + Sub);
+console.log('The Multiplication of ' + Num1 + ' and ' + Num2 + ' is: ' + Mul);
+console.log('The Division of ' + Num1 + ' and ' + Num2 + ' is: ' + Div);
---
+++ week1/week1program2.js
@@ -0,0 +1,12 @@
+const num1 = 10;
+const num2 = 5;
+
+const add = num1 + num2;
+const subtract = num1 - num2;
+const multiply = num1 * num2;
+const divide = num1 / num2;
+
+console.log("Addition: " + add);
+console.log("Subtraction: " + subtract);
+console.log("Multiplication: " + multiply);
+console.log("Division: " + divide);
