00de8d221b6253608b3039f74bbf70b9191a5fbf
prayagprasad <145901630+prayagprasad@users.noreply.github.com>
2024-12-21 05:24:59 +0000

    Add files via upload

---
+++ week5/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import AddNumbers from './AddNumbers'; 
+
+ReactDOM.render(
+  <AddNumbers num1={5} num2={10} />, 
+  document.getElementById('root')
+);
